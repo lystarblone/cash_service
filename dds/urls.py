394f44e70d53563c369be76e9d4f1f01fa3f71a4
Ivan Laptev <ivan.laptev952@gmail.com>
2025-04-29 14:59:01 +0300

    Add urls but not sure

--- dds/urls.py
+++ dds/urls.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 from django.urls import path
+from . import views
+
+app_name = 'dds'
 
 urlpatterns = [
-    
+    path('', views.index, name='index'),
+    path('transaction/create/', views.transaction_create, name='transaction_create'),
+    path('transaction/edit/<int:pk>/', views.transaction_edit, name='transaction_edit'),
+    path('transaction/delete/<int:pk>', views.transaction_delete, name='transaction_delete'),
+    path('dictionaries/', views.dictionaries, name='dictionaries'),
 ]
